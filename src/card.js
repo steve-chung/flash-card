2a72855ea13da8ef47576e8856ade6435010bad0
Steve Chung <heeman_1@hotmail.com>
2018-10-03 11:26:52 -0700

    create card and card list

---
+++ src/card.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Card = (props) => (
+  <div>
+    <p> {props.question} </p>
+  </div>
+)
+
+export default Card
--- src/cards.js
+++ src/cards.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
-// import Card from './card'
+import Card from './card'
 
 export default class Cards extends Component {
   // constructor(props) {
   //   super(props)
   // }
   render() {
-    // const { cards } = this.props
+    const { cards } = this.props
+    const cardList = cards.map((card) => (
+      <Card key={card.id} question={card.question} />
+    ))
 
     return (
       <div>
-
+        {cardList}
       </div>
     )
 
