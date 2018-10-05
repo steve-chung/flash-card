1cf18953e6d32a2b40880e64f998e1bc924b5615
Steve Chung <heeman_1@hotmail.com>
2018-10-05 16:14:44 -0700

    #14 removed unused variables, move the style to globle scope, and rennamed the variable to more readable

--- src/cardslide.js
+++ src/cardslide.js
@@ -1,42 +1,48 @@
 import React, {Component} from 'react'
 
+const style = {
+  arrow: {
+    fontSize: '2rem',
+    transform: 'rotate(90deg)',
+    transition: 'transform 1s ease-out'
+  },
+  answer: {
+    transform: 'scale(1, 1)',
+    transition: 'transform 600ms cubic-bezier(0.68, -0.55, 0.265, 1.55)'
+  },
+  progress: {
+    height: '1.5rem'
+  }
+}
+
 export default class CardSlide extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      isClicked: false
+      showAnswer: false
     }
     this.handleClick = this.handleClick.bind(this)
-    this.handleOnClick = this.handleOnClick.bind(this)
+    this.toggleShowAnswer = this.toggleShowAnswer.bind(this)
   }
 
-  handleOnClick() {
+  toggleShowAnswer() {
     this.setState((state) => ({
-      isClicked: !state.isClicked
+      showAnswer: !state.showAnswer
     }))
   }
 
   handleClick(direction) {
     this.props.onClick(direction)
     this.setState({
-      isClicked: false
+      showAnswer: false
     })
   }
 
   render() {
     const {question, answer} = this.props.cardInfo
     const {cardCount, page} = this.props
-    const {isClicked} = this.state
+    const {showAnswer} = this.state
     const progressWidth = ((page + 1) === cardCount) ? 100 : parseInt((100 / cardCount), 10) * (page + 1)
-    const styleArrow = {
-      fontSize: '2rem',
-      transform: 'rotate(90deg)',
-      transition: 'transform 1s ease-out'
-    }
-    const styleAnswer = {
-      transform: 'scale(1, 1)',
-      transition: 'transform 600ms cubic-bezier(0.68, -0.55, 0.265, 1.55)'
-    }
 
     return (
       <div className='card-container'>
@@ -44,11 +50,11 @@ export default class CardSlide extends Component {
           <div className='card-slide position-absolute'>
             <div className='d-inline-block slide-card border border-secondary'>
               <p className='text-center question p-3'>Q: {question}</p>
-              <i className="fas fa-arrow-circle-right" style={isClicked ? styleArrow : {fontSize: '2rem'}} onClick={this.handleOnClick}></i><span> See Answer</span>
-              <h4 className='text-center bg-danger answer text-white align-middle' style={isClicked ? styleAnswer : {transform: 'scale(1, 0)'} }>A: {answer}</h4>
+              <i className="fas fa-arrow-circle-right" style={showAnswer ? style.arrow : {fontSize: '2rem'}} onClick={this.toggleShowAnswer}></i><span> See Answer</span>
+              <h4 className='text-center bg-danger answer text-white align-middle' style={showAnswer ? style.answer : {transform: 'scale(1, 0)'} }>A: {answer}</h4>
             </div>
           </div>
-          <div className="progress" style={{height: '1.5rem'}}>
+          <div className="progress" style={style.progress}>
             <div className="progress-bar" role="progressbar" style={{width: `${progressWidth}%`}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">{progressWidth}%</div>
           </div>
         </div>
--- src/practice.js
+++ src/practice.js
@@ -5,66 +5,42 @@ export default class Practice extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      page: 0,
-      translateValue: 0,
-      lastTransValue: 0
+      page: 0
     }
     this.handleChangePage = this.handleChangePage.bind(this)
   }
 
-  calculateLastPageView() {
-    const {cards} = this.props
-    const cardLength = cards.length
-    const newLastTransValue = (cardLength - 1) * this.slideWidth()
-    this.setState({
-      lastTransValue: -newLastTransValue
-    })
-  }
-
-  componentDidMount() {
-    this.calculateLastPageView()
-  }
-
-  slideWidth() {
-    return document.querySelector('.practice').clientWidth
-  }
-
   handleChangePage(direction) {
-    let {page, translateValue, lastTransValue} = this.state
+    let {page} = this.state
     const {cards} = this.props
     const cardLength = cards.length
     switch (direction) {
       case 'left':
         if (page > 0) {
           page -= 1
-          translateValue = translateValue + this.slideWidth()
         }
         else {
           page = cardLength - 1
-          translateValue = lastTransValue
         }
         break
       case 'right':
         if (page < cardLength - 1) {
           page += 1
-          translateValue = translateValue - this.slideWidth(0)
         }
         else {
           page = 0
-          translateValue = 0
         }
         break
     }
     this.setState({
-      page,
-      translateValue
+      page
     })
   }
   render() {
-    const {page, translateValue} = this.state
+    const {page} = this.state
     const {cards} = this.props
     return (
-      <CardSlide page={page} translateValue={translateValue} cardInfo={cards[page]} onClick={this.handleChangePage} cardCount={cards.length}/>
+      <CardSlide page={page} cardInfo={cards[page]} onClick={this.handleChangePage} cardCount={cards.length}/>
     )
   }
 
